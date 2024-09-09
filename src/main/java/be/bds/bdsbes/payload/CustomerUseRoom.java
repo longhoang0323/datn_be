0bad108c92a25d06e01c555254dc6deeeefcda0b
khanhdq <khanhdq2411@gmail.com>
2024-09-09 23:44:12 +0700

    update code

--- src/main/java/be/bds/bdsbes/payload/CustomerUseRoom.java
+++ src/main/java/be/bds/bdsbes/payload/CustomerUseRoom.java
@@ -5,16 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerUseRoom {
 
+    public Long idPhong;
     public String maPhong;
     public String tenKhachHang;
     public String sdt;
     public String cccd;
     public Integer trangThaiDatPhong;
+    public LocalDateTime checkIn;
+    public LocalDateTime checkOut;
 
 }
